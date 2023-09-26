b89b7aa9c7dbc2277207cb9754bb82d90a752115
Minh-Ngoc <ngocminh101100@gmail.com>
2023-09-26 20:56:47 +0700

    fix bug deploy

--- src/redux/services/productService.ts
+++ src/redux/services/productService.ts
@@ -1,20 +1,20 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { URL, GET_PRODUCT_LIST, GET_PRODUCT_FILTER, DISPATCH_PRODUCT_ID } from '@/api';
+// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+// import { URL, GET_PRODUCT_LIST, GET_PRODUCT_FILTER, DISPATCH_PRODUCT_ID } from '@/api';
 
-export const userApi = createApi({
-  reducerPath: "userApi",
-  refetchOnFocus: true,
-  baseQuery: fetchBaseQuery({
-    baseUrl: `${URL}/`,
-  }),
-  endpoints: (builder) => ({
-    getUsers: builder.query<User[], null>({
-      query: () => "users",
-    }),
-    getUserById: builder.query<User, { id: string }>({
-      query: ({ id }) => `users/${id}`,
-    }),
-  }),
-});
+// export const userApi = createApi({
+//   reducerPath: "userApi",
+//   refetchOnFocus: true,
+//   baseQuery: fetchBaseQuery({
+//     baseUrl: `${URL}/`,
+//   }),
+//   endpoints: (builder) => ({
+//     getUsers: builder.query<User[], null>({
+//       query: () => "users",
+//     }),
+//     getUserById: builder.query<User, { id: string }>({
+//       query: ({ id }) => `users/${id}`,
+//     }),
+//   }),
+// });
 
-export const { useGetUsersQuery, useGetUserByIdQuery } = userApi;
+// export const { useGetUsersQuery, useGetUserByIdQuery } = userApi;
